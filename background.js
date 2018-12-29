dff51812aafb2236fff01dfe079b0829331eae1c
Kaleo <kaleocheng@gmail.com>
2018-12-29 11:10:16 +0800

    Use updateTabNumber function

--- background.js
+++ background.js
@@ -1,28 +1,38 @@
 const titles = {}
 
+function updateTabNumber() {
+    chrome.tabs.query({}, function (tabs) {
+        tabs.forEach((tab, i, tabs) => {
+            if (!(tab.id in titles)) {
+                titles[tab.id] = tab.title
+            }
+            if (tab.index < 8) {
+                chrome.tabs.executeScript(tab.id, {
+                    code: `document.title = '⌘${tab.index + 1} ${titles[tab.id]}'`
+                })
+            } else if (i === tabs.length - 1) {
+                chrome.tabs.executeScript(tab.id, {
+                    code: `document.title = '⌘9 ${titles[tab.id]}'`
+                })
+            } else {
+                chrome.tabs.executeScript(tab.id, {
+                    code: `document.title = '${titles[tab.id]}'`
+                })
+            }
+
+        })
+    })
+}
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status == 'complete') {
-        chrome.tabs.query({}, function (tabs) {
-            tabs.forEach((tab, i, tabs) => {
-                if (!(tab.id in titles)) {
-                    titles[tab.id] = tab.title
-                }
-                if (tab.index < 8) {
-                    chrome.tabs.executeScript(tab.id, {
-                        code: `document.title = '⌘${tab.index + 1} ${titles[tab.id]}'`
-                    })
-                } else if (i === tabs.length - 1) {
-                    chrome.tabs.executeScript(tab.id, {
-                        code: `document.title = '⌘9 ${titles[tab.id]}'`
-                    })
-                } else {
-                    chrome.tabs.executeScript(tab.id, {
-                        code: `document.title = '${titles[tab.id]}'`
-                    })
-                }
-
-            })
-        })
+        updateTabNumber()
     }
+})
+
+chrome.runtime.onInstalled.addListener(updateTabNumber)
+
+chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
+    updateTabNumber()
 })
 
